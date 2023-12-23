e5f7fbde019d4284a634e28d8183d423476881bd
Maksim Bersenev <106951162+Bersenev787@users.noreply.github.com>
2023-12-23 13:47:59 +0300

    feat: add main page

--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,7 +1,20 @@
+import { FitnessCard } from '../../components/fitness-card/fitness-card';
 import { Header } from '../../components/header/header';
 import './main-page.scss';
 
 export const MainPage = () => {
+    const fitnessCards = [
+        {title: 'Йога', img: 'card1'},
+        {title: 'Стретчинг', img: 'card2'},
+        {title: 'Танцевальный фитнес', img: 'card3'},
+        {title: 'Степ-аэробика', img: 'card4'},
+        {title: 'Бодифлекс', img: 'card5'},
+    ];
+
+    const handlerScrollToTop = () => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    };
+
     return (
         <main className='main-page'>
             <div className='container'>
@@ -17,6 +30,14 @@ export const MainPage = () => {
                     </div>
                     <img src="/img/sale-sticker.svg" alt="sale-sticker" className='main-page__info-img' />
                 </div>
+                <div className="main-page__cards">
+                    {
+                        fitnessCards.map(({title, img}) => {
+                            return <FitnessCard title={title} img={img} key={img} />
+                        })
+                    }
+                </div>
+                <button className='main-page__button-up' onClick={handlerScrollToTop}>Наверх ↑</button>
             </div>
         </main>
     );
