c8c30b9eb1d7ad939424f413014287ce66b122b5
Anton <246shustrica@mail.ru>
2023-12-31 04:22:23 +0500

    feat: Firebase installed

---
+++ Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8e7GnmiSc0s_okadPpDPwlNSYbjaJIFQ",
+  authDomain: "fitnes-pro-a9533.firebaseapp.com",
+  databaseURL: "https://fitnes-pro-a9533-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fitnes-pro-a9533",
+  storageBucket: "fitnes-pro-a9533.appspot.com",
+  messagingSenderId: "430839690751",
+  appId: "1:430839690751:web:4041418114d7776303a3ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
