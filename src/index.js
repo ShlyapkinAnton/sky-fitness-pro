dbffa2db8402890e2fd308c98271bf79f87afa0b
Anton <246shustrica@mail.ru>
2024-01-21 20:29:44 +0500

    modified:   src/Firebase.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { GlobalStyle } from "./styles/common.styles";
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './components/scroll-to-top/scroll-to-top';
-import './firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
